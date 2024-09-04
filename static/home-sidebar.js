3c8ced0cb4de084601f6483fdb6ad1cee55f4d9b
chienhuak <christy.kch.c@gmail.com>
2024-09-05 03:25:04 +0800

    fix / UI : adjust sidebar show / hide control

--- static/home-sidebar.js
+++ static/home-sidebar.js
@@ -1,9 +1,15 @@
-function toggleSidebar() {
+let isSidebarOpen
+function toggleSidebar(isOpen) {
 	const sidebar = document.getElementById("mySidebar");
-	if (sidebar.style.width === "200px") {
-	  sidebar.style.width = "0";
+	if(!sidebar.style.width) isSidebarOpen = isOpen;
+	if (isSidebarOpen) {
+		sidebar.style.width = "0";
+		isSidebarOpen = false;
+		console.log('sidebar close',sidebar.style.width);
 	} else {
-	  sidebar.style.width = "200px";
+		sidebar.style.width = "200px";
+		isSidebarOpen = true;
+	    console.log('sidebar open',sidebar.style.width);
 	}
 }
 
